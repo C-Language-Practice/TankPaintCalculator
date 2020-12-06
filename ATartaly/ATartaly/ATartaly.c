46cbb665bfb504f497a758390b5402e5d999f948
Krisz Kaszas <kriszrokakaszas@gmail.com>
2020-12-06 13:52:11 +0100

    done

---
+++ ATartaly/ATartaly/ATartaly.c
@@ -0,0 +1,26 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+float calcSurfaceArea(float height, float radius)
+{
+	const float Pi = 3.14159265359;
+	return 2 * Pi * radius * height + 2 * Pi * (radius * radius);
+}
+
+float calcPaintCanNum(float height, float radius)
+{
+	return calcSurfaceArea(height, radius)/2;
+}
+
+int main(void)
+{
+	float height, radius;
+
+	printf_s("Please specify tank height:\n");
+	scanf_s("%f", &height);
+	printf_s("Please specify tank radius:\n");
+	scanf_s("%f", &radius);
+	
+	printf_s("%f cans of paint are necessary to paint the tank.", calcPaintCanNum(height, radius));
+	return 0;
+}
